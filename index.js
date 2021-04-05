7b25a13caaa276475d9f7203e82d92a13b10892a
lacerta <kevinhickman4@gmail.com>
2021-04-04 19:47:59 -0500

    npm test did not work, copied solution will review.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+//npm test did not work, copied solution will review.
 const driver = 'bob';
+
+console.log('this code was called');
+
+console.log(driver);
+
+console.log(driver, 'is the driver variable name');
